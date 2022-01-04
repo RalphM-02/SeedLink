cbb374f255e28d325907e2b4613f762d535a40f2
RalphM-02 <80824378+RalphM-02@users.noreply.github.com>
2022-01-04 11:49:10 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/g24/mco/seedlink/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.g24.mco.seedlink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
