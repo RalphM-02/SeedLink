524bddb6c0223c8f4cc4448100e902245e688220
RalphM-02 <80824378+RalphM-02@users.noreply.github.com>
2022-02-15 17:25:44 +0800

    Post Class

--- app/src/main/java/ph/edu/dlsu/mobdeve/g24/mco/seedlink/PostClass.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/g24/mco/seedlink/PostClass.kt
@@ -1,5 +1,25 @@
 package ph.edu.dlsu.mobdeve.g24.mco.seedlink
 
-data class PostClass {
-    var author =
+data class PostClass (
+        var author: String,
+        var image: ByteArray,
+        var caption: String
+        ) {
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as PostClass
+
+                if (author != other.author) return false
+                if (!image.contentEquals(other.image)) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = author.hashCode()
+                result = 31 * result + image.contentHashCode()
+                return result
+        }
 }
